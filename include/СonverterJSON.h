0ea4b6b00db665f3b54af440e622d282f650be49
sunlight017 <skuf5017@gmail.com>
2025-08-08 15:08:33 +0300

    Update СonverterJSON.h

--- include/СonverterJSON.h
+++ include/СonverterJSON.h
@@ -1,23 +1,26 @@
 #ifndef CONVERTERJSON_H
 #define CONVERTERJSON_H
 
+#include <nlohmann/json.hpp>
 #include <vector>
 #include <string>
-#include <nlohmann/json.hpp>
+#include <filesystem>
 
 class ConverterJSON {
 public:
     ConverterJSON();
+
     std::vector<std::string> GetTextDocuments();
     int GetResponsesLimit();
     std::vector<std::string> GetRequests();
-    void putAnswers(std::vector<std::vector<std::pair<int, float>>> answers);
+    void putAnswers(const std::vector<std::vector<std::pair<size_t, float>>>& answers);
 
 private:
     nlohmann::json config;
     nlohmann::json requests;
+
     void validateConfig();
     std::string readFile(const std::string& path);
 };
 
-#endif // CONVERTERJSON_H
+#endif
