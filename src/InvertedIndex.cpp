932fa731a0fdf782857c8a3fd87c8c7cfd556221
sunlight017 <skuf5017@gmail.com>
2025-08-08 15:10:18 +0300

    Update InvertedIndex.cpp

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -1,14 +1,14 @@
 #include "InvertedIndex.h"
-#include <algorithm>
 #include <sstream>
-#include <thread>
+#include <cctype>
 
 void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs) {
+    std::lock_guard<std::mutex> lock(docs_mutex);
     docs = std::move(input_docs);
     freq_dictionary.clear();
 
     std::vector<std::thread> threads;
-    for (size_t doc_id = 0; doc_id < docs.size(); ++doc_id) {
+    for (size_t doc_id = 0; doc_id < docs.size(); doc_id++) {
         threads.emplace_back(&InvertedIndex::indexDocument, this, doc_id, docs[doc_id]);
     }
 
@@ -22,22 +22,19 @@ void InvertedIndex::indexDocument(size_t doc_id, const std::string& text) {
     std::map<std::string, size_t> wordCounts;
 
     for (const auto& word : words) {
-        ++wordCounts[word];
+        wordCounts[word]++;
     }
 
-    std::lock_guard<std::mutex> lock(dict_access);
+    std::lock_guard<std::mutex> lock(docs_mutex);
     for (const auto& [word, count] : wordCounts) {
         freq_dictionary[word].push_back({ doc_id, count });
     }
 }
 
 std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word) {
-    std::lock_guard<std::mutex> lock(dict_access);
+    std::lock_guard<std::mutex> lock(docs_mutex);
     auto it = freq_dictionary.find(word);
-    if (it != freq_dictionary.end()) {
-        return it->second;
-    }
-    return {};
+    return it != freq_dictionary.end() ? it->second : std::vector<Entry>{};
 }
 
 std::vector<std::string> InvertedIndex::splitIntoWords(const std::string& text) {
@@ -48,9 +45,11 @@ std::vector<std::string> InvertedIndex::splitIntoWords(const std::string& text) 
     while (iss >> word) {
         word.erase(std::remove_if(word.begin(), word.end(),
             [](char c) { return !isalpha(c); }), word.end());
+
         std::transform(word.begin(), word.end(), word.begin(), ::tolower);
 
-        if (!word.empty()) {
+
+        if (!word.empty() && word.length() <= 100) {
             words.push_back(word);
         }
     }
